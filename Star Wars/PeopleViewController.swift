02281ca3c2d0da7e2c81ff1f359edf41a039623f
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2017-07-16 15:04:36 -0700

    initial commit

---
+++ Star Wars/PeopleViewController.swift
@@ -0,0 +1,75 @@
+//
+//  PeopleViewController.swift
+//  Star Wars
+//
+//  Created by Lu Yu on 7/14/17.
+//  Copyright © 2017 Lu Yu. All rights reserved.
+//
+
+import UIKit
+class PeopleViewController: UITableViewController {
+    // Hardcoded data for now
+    var people = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getData()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // if we return - sections we won't have any sections to put our rows in
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the count of people in our data array
+        return people.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a generic cell
+        let cell = UITableViewCell()
+        // set the default cell label to the corresponding element in the people array
+        cell.textLabel?.text = people[indexPath.row]
+        // return the cell so that it can be rendered
+        return cell
+    }
+    func getData() {
+        let url = URL(string: "http://swapi.co/api/people/")
+        // create a URLSession to handle the request tasks
+        let session = URLSession.shared
+        // create a "data task" to make the request and run the completion handler
+        let task = session.dataTask(with: url!, completionHandler: {
+            // see: Swift closure expression syntax
+            data, response, error in
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    if let results = jsonResult["results"] {
+                        // coercing the results object as an NSArray and then storing that in resultsArray
+                        let resultsArray = results as! NSArray
+                        // now we can run NSArray methods like count and firstObject
+                        for person in resultsArray {
+                            // cast to dictionary for data extraction
+                            let personDict = person as! NSDictionary
+                            self.people.append(personDict["name"]! as! String)
+                        }
+//                        print(resultsArray.count)
+//                        print(resultsArray[0])
+//                        if let obj = resultsArray.firstObject {
+//                            print(obj)
+//                        }
+                    }
+                }
+                self.tableView.reloadData()
+            } catch {
+                print(error)
+            }
+        })
+        // execute the task and wait for the response before
+        // running the completion handler. This is async!
+        task.resume()
+        print("I happen before the response!")
+    }
+}
+
